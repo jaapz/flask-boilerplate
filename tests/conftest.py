c20208504215b218defdd132b6e1d7ed569d50b0
Jaap Broekhuizen <jaapz.b@gmail.com>
2013-09-10 12:01:56 +0200

    Fix tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
-from app import app
+from app import app as testable_app
 
 
 @pytest.fixture
 def app():
-    return app
+    return testable_app
 
 
 @pytest.fixture
-def client():
+def client(app):
     return app.test_client()
